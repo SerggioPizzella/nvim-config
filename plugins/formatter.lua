dd7081d3f27e01996d6a819311099d28edfc20e0
Serggio Pizzella <serggiopizzella@gmail.com>
2024-08-13 16:01:24 +0200

    default formatter

--- plugins/formatter.lua
+++ plugins/formatter.lua
@@ -8,16 +8,19 @@ return {
         ---@type conform.setupOpts
         local config = {
             notify_on_error = false,
+            default_format_opts = {
+                async = true,
+                lsp_format = "fallback",
+            },
             formatters_by_ft = {
                 javascript = { "prettierd", "prettier", stop_after_first = true },
                 typescript = { "prettierd", "prettier", stop_after_first = true },
                 typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-            }
+                ["_"] = { "trim_whitespace" },
+            },
         }
         require('conform').setup(config)
 
-        vim.keymap.set('n', '<leader>f', function()
-            require('conform').format({ async = true, lsp_format = "fallback" })
-        end, { desc = '[F]ormat buffer' })
+        vim.keymap.set('n', '<leader>f', require('conform').format, { desc = '[F]ormat buffer' })
     end,
 }
